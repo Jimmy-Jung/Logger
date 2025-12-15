013e9fc88e20bcbc9c28a1bcf856d4d8186ec1b6
jimmy <junyoung.jung@megastudy.net>
2025-12-15 18:41:29 +0900

    feat: [로거-연동] Firebase 로그 목적지 구현
    - AnalyticsLogDestination 구현 (Firebase Analytics 연동)
    - CrashlyticsLogDestination 구현 (Firebase Crashlytics 연동)
    - Firebase SDK를 통한 로그 및 크래시 리포트 전송 기능

---
+++ Projects/LoggerFirebase/Project.swift
@@ -0,0 +1,34 @@
+// Project.swift
+// LoggerFirebase
+//
+// Created by jimmy on 2025-12-15.
+
+import ProjectDescription
+
+let project = Project(
+    name: "LoggerFirebase",
+    organizationName: "com.logger",
+    packages: [
+        .remote(
+            url: "https://github.com/firebase/firebase-ios-sdk",
+            requirement: .upToNextMajor(from: "10.0.0")
+        )
+    ],
+    targets: [
+        .target(
+            name: "LoggerFirebase",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "com.logger.LoggerFirebase",
+            deploymentTargets: .iOS("15.0"),
+            infoPlist: .default,
+            sources: ["Sources/**"],
+            dependencies: [
+                .project(target: "Logger", path: "../Logger"),
+                .package(product: "FirebaseCrashlytics"),
+                .package(product: "FirebaseAnalytics")
+            ]
+        )
+    ]
+)
+
